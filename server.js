6ffd7393427b0cdb837de15fda021098bd36eb57
Erfan <erfanjaliliyan83@gmail.com>
2025-11-10 03:45:30 -0800

    debug the server

--- server.js
+++ server.js
@@ -19,12 +19,13 @@ const productRoutes = require('./routes/products');
 app.use('/api/products', productRoutes);
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+// فعلاً چون MongoDB نصب یا اتصال نداری، این بخش کامنت شده
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+// .then(() => console.log('MongoDB connected'))
+// .catch(err => console.log(err));
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
